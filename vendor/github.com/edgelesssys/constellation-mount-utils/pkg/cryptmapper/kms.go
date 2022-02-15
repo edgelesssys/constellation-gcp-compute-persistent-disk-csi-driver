58898441169d261bd74e9129c225ebcc6313360e
Daniel Weiße <dw@edgeless.systems>
2022-02-15 10:59:39 +0100

    Update vendor directory
    Signed-off-by: Daniel Weiße <dw@edgeless.systems>

---
+++ vendor/github.com/edgelesssys/constellation-mount-utils/pkg/cryptmapper/kms.go
@@ -0,0 +1,12 @@
+package cryptmapper
+
+import (
+	"context"
+
+	"github.com/edgelesssys/constellation-kms-client/pkg/kms"
+)
+
+type kmsClient interface {
+	GetDEK(ctx context.Context, kekID, dekID string) ([]byte, error)
+	CreateKEK(ctx context.Context, keyID string, kek []byte, policyProducer kms.KeyPolicyProducer) ([]byte, error)
+}
