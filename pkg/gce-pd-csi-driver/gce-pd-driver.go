31b2c170987c90c5fb51c13e1d6e6e5298e4e25f
Daniel Weiße <dw@edgeless.systems>
2022-05-19 12:37:22 +0200

    Remove multi-writer capability
    Signed-off-by: Daniel Weiße <dw@edgeless.systems>

--- pkg/gce-pd-csi-driver/gce-pd-driver.go
+++ pkg/gce-pd-csi-driver/gce-pd-driver.go
@@ -57,7 +57,7 @@ func (gceDriver *GCEDriver) SetupGCEDriver(name, vendorVersion string, extraVolu
 	vcam := []csi.VolumeCapability_AccessMode_Mode{
 		csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER,
 		csi.VolumeCapability_AccessMode_MULTI_NODE_READER_ONLY,
-		csi.VolumeCapability_AccessMode_MULTI_NODE_MULTI_WRITER,
+		csi.VolumeCapability_AccessMode_MULTI_NODE_SINGLE_WRITER,
 	}
 	gceDriver.AddVolumeCapabilityAccessModes(vcam)
 	csc := []csi.ControllerServiceCapability_RPC_Type{
